ece6a1189e86cf8a7300fb5fa67b5d37ceea082d
gazal2708 <your-email@example.com>
2025-07-11 11:44:24 +0530

    added decorate image method

--- scripts/aem.js
+++ scripts/aem.js
@@ -270,6 +270,10 @@ async function loadCSS(href) {
   });
 }
 
+function getUrlExtension(url) {
+  return url.split(/[#?]/)[0].split('.').pop().trim();
+}
+
 /**
  * Checks if an element is an external image.
  * @param {Element} element The element
